7c9e38073dc6d1258c0eec17842a3479da35a00b
Diontae Akins <diontaea@gmail.com>
2024-04-04 15:52:53 -0400

    3

--- js/scripts.js
+++ js/scripts.js
@@ -39,10 +39,21 @@ let firstVariable = ' Hello World'
  console.log(c > d);
  console.log('Name' === 'Name');
  // FOR THE NEXT TWO, USE ONLY && OR ||
- console.log(true && false);
+ console.log(true || false);
  console.log(false || false || false || false || false || true);
- console.log(false && false)
+ console.log(false === false)
  console.log(e === 'Kevin');
  console.log(a + b === c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
  console.log(a * a === d); // note: the answer is a simple arithmetic equation, not something "weird"
  console.log(48 == '48');
+
+ //Biggie Smalls
+let num = 100
+
+ if (num < 100) {
+    console.log('lil number');
+   
+} else if (num >= 100) {
+    console.log('Big Dog');
+ }
+   

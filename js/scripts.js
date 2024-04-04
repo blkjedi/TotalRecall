6692a624a2c4ab784114c7fed2df32db1ce9bae2
Diontae Akins <diontaea@gmail.com>
2024-04-04 15:33:02 -0400

    2

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,48 @@
-console.log(running);
+console.log('running');
 
 // varible and data types
 
 const myName = 'jedi'
+
+
+let newFavoriteLanguage= 'JavaScript'
+let favoriteLanguage = newFavoriteLanguage
+
+console.log('hi my name is ',myName, 'and my favorite language is', favoriteLanguage);
+
+
+//============ strings
+let firstVariable = ' Hello World' 
+ firstVariable =  15 
+
+ let secondVariable = firstVariable
+
+ secondVariable = 'broooo'
+
+ console.log(firstVariable);
+
+ let yourName = "john"
+
+ console.log('Hello, my name is', yourName);
+
+
+
+ // Booleans
+
+ const a = 4;
+ const b = 53;
+ const c = 57;
+ const d = 16;
+ const e = 'Kevin';
+
+ console.log(a < b);
+ console.log(c > d);
+ console.log('Name' === 'Name');
+ // FOR THE NEXT TWO, USE ONLY && OR ||
+ console.log(true && false);
+ console.log(false || false || false || false || false || true);
+ console.log(false && false)
+ console.log(e === 'Kevin');
+ console.log(a + b === c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+ console.log(a * a === d); // note: the answer is a simple arithmetic equation, not something "weird"
+ console.log(48 == '48');
